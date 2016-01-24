5172df3914bc21ff949f2c2f3d5d39f3066f511f
Mansi <mpatel.y@gmail.com>
2016-01-24 17:51:59 -0500

    Initial Commit

---
+++ server.ts
@@ -0,0 +1,11 @@
+import http = require('http');
+
+
+var port:number = process.env.PORT || 8080;
+
+http.createServer(function(req:http.ServerRequest, res:http.ServerResponse){
+    res.writeHead(200, { 'Content-Type': 'text/plain'});
+    res.end("Hello World\n");
+}).listen(port);
+
+
